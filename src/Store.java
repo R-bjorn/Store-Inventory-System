650fba6673cb8d336fede213ef88ce8bad7493d6
r-bjorn <ravidtrivedi111@gmail.com>
2021-10-14 22:40:24 -0400

    Create a Store
    Creating a class called Store and implementing variables and methods

---
+++ src/Store.java
@@ -0,0 +1,74 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Store {
+    ArrayList<Item> inventory;
+    double totalSalesValue = 0.0;
+    int totalNumberOfSales = 0;
+
+    // Constructor
+    Store(){
+        inventory = new ArrayList<Item>();
+    }
+
+    // Methods
+    public void addItem(Item newItem){
+        inventory.add(newItem);
+    }
+
+    private boolean makeSale(int itemId, int quantity, LocalDate sellDate){
+        Item item = getItemByID(itemId);
+        if(inventory.isEmpty() || getItemByID(itemId) == null){
+            return false;
+        }
+        double value = item.sell(quantity, sellDate);
+        if(value > 0){
+            totalNumberOfSales++;
+            totalSalesValue += value;
+        }
+        return false;
+    }
+
+    private String getInventoryString(){
+        String inventoryString = "";
+        for(Item item : inventory){
+            inventoryString += "\n" + item.toString() + "\n";
+        }
+        return inventoryString;
+    }
+
+    private void listInventory(){
+        System.out.println("Here are all the inventory items:");
+        System.out.println(getInventoryString());
+    }
+
+    public Item checkForItem(int itemId){
+        for(Item e : inventory){
+            if (e.getItemID() == itemId){
+                return e.makeCopy();
+            }
+        }
+        return null;
+    }
+
+    public Item getItemByID(int itemId){
+        for(Item e : inventory){
+            // Item's properties can be modified by store class
+            // because we are returning the real copy of the item (original memory address)
+            return e;
+        }
+        return null;
+    }
+
+    public Double getTotalSalesValue(){
+        return totalSalesValue;
+    }
+
+    public int getTotalNumberOfSales(){
+        return totalNumberOfSales;
+    }
+
+    public String toString(){
+        return "Total sales value: " + totalSalesValue + "Total number of sales: " + totalNumberOfSales + "Current inventory" + getInventoryString();
+    }
+}
