c43c56c0a2a64565ac81b60fd8f1286c9e228122
r-bjorn <ravidtrivedi111@gmail.com>
2021-10-15 10:39:11 -0400

    Creating Book
    Creating a class called Book, part of the Item class
    Adding Instance variables and methods

---
+++ src/Book.java
@@ -0,0 +1,44 @@
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.ArrayList;
+
+public class Book extends Item {
+        ArrayList<String> authors;
+
+        // Constructors
+        public Book(String title, double regularPrice, LocalDate releaseDate, int quantity, ArrayList<String> authors){
+            super(title,regularPrice,releaseDate,quantity);
+            this.authors = authors;
+        }
+        public Book(String title, double regularPrice, LocalDate releaseDate, int quantity, ArrayList<String> authors, int itemID){
+            super(title, regularPrice, releaseDate, quantity, itemID);
+            this.authors = authors;
+        }
+
+        // Methods
+        public ArrayList<String> getAuthors(){
+            return (ArrayList<String>) authors.clone();
+        }
+
+        public String toString(){
+            StringBuilder author = new StringBuilder();
+            for(String s : authors) author.append("\n\t").append(s);
+            return super.toString()  + "Authors:" + author;
+        }
+
+        public double getPrice(LocalDate sellDate){
+            return regularPrice - (regularPrice * discount);
+        }
+
+        public Object makeCopy(){
+            try {
+                return this.clone();
+            } catch (CloneNotSupportedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+
+
+}
