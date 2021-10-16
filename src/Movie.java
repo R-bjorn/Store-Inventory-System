693014d0f8d87e179054ff480576e5fa0a97dcc5
r-bjorn <ravidtrivedi111@gmail.com>
2021-10-16 00:16:10 -0400

    Adding COnstructors

---
+++ src/Movie.java
@@ -0,0 +1,16 @@
+import java.time.LocalDate;
+
+public class Movie extends Item{
+    String director;
+    String producer;
+
+    public Movie(String title, double regularPrice, LocalDate releaseDate, int quantity, String director, String producer){
+        super(title, regularPrice, releaseDate, quantity);
+        this.director = director;
+        this.producer = producer;
+    }
+//  Only for copying
+//    public Movie(){
+//
+//    }
+}
